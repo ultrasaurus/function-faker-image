55caa550b9e977b68ce0115e6838ab7737aaac5a
Sarah Allen <sarahallen@google.com>
2018-04-14 16:17:42 -0700

    storage config

---
+++ README.md
@@ -0,0 +1,6 @@
+
+```
+firebase functions:config:set project.id=fractal-example
+firebase functions:config:set storage.bucket=fractal-example.appspot.com
+```
+
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,30 @@
 import * as functions from 'firebase-functions';
 
+import * as admin from 'firebase-admin';
+//import * as Storage from '@google-cloud/storage';
+import Storage = require('@google-cloud/storage');
+
+admin.initializeApp(functions.config().firebase);
+
+function getConfig() {
+  const config = functions.config();
+  console.log('using config', config);
+   return config;
+}
+
+export const newImage = functions.https.onRequest((request, response) => {
+  const config = getConfig();
+  const storage = Storage({
+    projectId: config.projectId,
+    keyFilename: 'storage-credential.json'
+  });
+  const bucket = storage.bucket(config.storage.bucket);
+
+
+  console.log('service account =', config.storage.credential)
+  response.send("Ok!");
+ });
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
