38333ccd2ce4d318a7552afd33740a67224832d3
Sarah Allen <sarahallen@google.com>
2018-04-15 04:22:58 -0700

    generates a fractal and saves in Google Cloud Storage

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 
+
+
+
 ```
-firebase functions:config:set project.id=fractal-example
-firebase functions:config:set storage.bucket=fractal-example.appspot.com
+PROJECT_ID=fractal-example
+firebase functions:config:set project.id=$PROJECT_ID
+firebase functions:config:set storage.bucket=$PROJECT_ID.appspot.com
+cd functions
+npm run build
+firebase deploy --only functions
+curl https://us-central1-$PROJECT_ID.cloudfunctions.net/newImage
+
+# to only deploy a single function
+firebase deploy --only functions:newImage
 ```
 
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,7 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import Storage = require('@google-cloud/storage');
 const spawn = require('child-process-promise').spawn;
+const fs = require('fs');
 const os = require('os');
 const path = require('path');
 
@@ -35,22 +36,37 @@ export const newImage = functions.https.onRequest((request, response) => {
     // 'Cache-Control': 'public,max-age=3600',
   };
 
-  let filePath = 'test-julia';
+  let filePath = 'test-julia2';
   const tempLocalFilePng = path.join(os.tmpdir(), filePath + '.png');
   const tempLocalFilePpm = path.join(os.tmpdir(), filePath + '.ppm');
 
   //const tempLocalFile = 'fractastic/examples/julia1.png';
   console.log('making', tempLocalFilePpm);
 
-  return spawn('./fractastic/fractastic', ['J', `${IMAGE_WIDTH}`, `${IMAGE_HEIGHT}`, '-2', '2', '-2', '2', '1000', '1', '-0.4', '0.6', '2>' , `${tempLocalFilePpm}`], { capture: [ 'stdout', 'stderr' ]})
-  .then(function (result) {
-    console.log('[spawn] stdout: ', result.stdout.toString());
-    return bucket.upload(tempLocalFilePpm, {destination: '/test/julia1.ppm', metadata: metadata}).then(() => {
-      return response.send("Ok!");
+  // `${tempLocalFilePpm}`
+  const pemFile = fs.createWriteStream(tempLocalFilePpm);
+  return spawn('./fractastic/fractastic',
+    ['J', `${IMAGE_WIDTH}`, `${IMAGE_HEIGHT}`,
+     '-2', '2', '-2', '2',
+     '1000', '1',
+     '-0.4', '0.6', '2'],
+    { capture: [ 'stdout', 'stderr' ]})
+  .then((result) => {
+    console.log('fractastic completed without error');
+    pemFile.write(result.stdout);
+    pemFile.end();
+    //    convert $output_file.ppm $output_file.png
+    return spawn('convert', [tempLocalFilePpm, tempLocalFilePng], { capture: [ 'stdout', 'stderr' ]})
+    .then((convertResult) => {
+      console.log('[spawn] stdout: ', convertResult.stdout.toString());
+      return bucket.upload(tempLocalFilePng, {destination: '/test/julia2.png', metadata: metadata}).then(() => {
+        return response.send("Ok!");
+      })
+      .catch(function (err) {
+        console.error('[bucket.upload] err: ', err);
+      });
+
     })
-    .catch(function (err) {
-      console.error('[bucket.upload] err: ', err);
-    });
   })
   .catch(function (err) {
     console.error('[spawn] err: ', err);
