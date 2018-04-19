f809c83f04b4b271d9f67d231d5bb47bc29ea52c
Sarah Allen <sarahallen@google.com>
2018-04-19 06:36:22 -0700

    add basic faker functionality

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from 'firebase-functions';
+import * as faker from 'faker'
 
 import * as admin from 'firebase-admin';
 import {ImageMaker} from './image-maker';
@@ -32,3 +33,46 @@ export const newImage = functions.https.onRequest((request, response) => {
 export const helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
+
+const COLLECTION_NAME = 'items';
+
+function addToCollection(obj: any) {
+  let config = getConfig();
+  let name = COLLECTION_NAME
+  console.log('name', name);
+  return admin.firestore()
+              .collection(name)
+              .add(obj).then(writeResult => {
+                let result = {id: writeResult.id, collection:name, doc: obj};
+                console.log(result);
+                return result;
+  });
+}
+
+export let addDocument = functions.https.onRequest((req, res) => {
+  console.log('addDocument query', req.query);
+  const name = req.query.name;
+  const value = req.query.value;
+  let obj = {};
+  obj[name] = value;
+
+  addToCollection(obj).then(result => {
+    // Send back a message that we've succesfully written the message
+    res.json(result);
+  });
+});
+
+export let addFake = functions.https.onRequest((req, res) => {
+  console.log('addFake query', req.query);
+  const name = req.query.name || 'text';
+  const value = faker.hacker.phrase();
+  let obj = {};
+  obj[name] = value;
+
+  addToCollection(obj).then(result => {
+    // Send back a message that we've succesfully written the message
+    res.json(result);
+  });
+});
+
+
