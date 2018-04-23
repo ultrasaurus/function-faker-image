9f3ec5b9b95bcc12eefef0fd3b2d6d4120e21ed4
Doug Stevenson <dougstevenson@google.com>
2018-04-22 21:38:00 -0700

    Upheaval
    image-maker only generates image, upload in http function
    store image in bucket with name based on Firestore document name
    consistent use of async / await
    
    rename storage-credential.json to serviceAccount.json
    read admin configuration from FIREBASE_CONFIG

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,121 +1,91 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-import * as faker from 'faker'
+import * as path from 'path'
+import * as fs from 'fs'
+import * as faker from 'faker';
 import { ImageMaker } from './image-maker';
+import * as serviceAccount from '../serviceAccount.json';
 
-admin.initializeApp();
+const adminConfig = JSON.parse(process.env.FIREBASE_CONFIG)
+adminConfig.credential = admin.credential.cert(<any>serviceAccount)
+admin.initializeApp(adminConfig);
 
-console.log(process.env)
-const PROJECT_ID = process.env.GCLOUD_PROJECT
-
-function getConfig() {
-  const config = functions.config();
-  console.log('using config', config);
-  return config;
-}
-
-function generateImage(options: any) {
-  const config = getConfig();
-  const bucketName =  config['storage'].bucket as string;
-  const fakeImage = new ImageMaker(PROJECT_ID, bucketName);
-  return fakeImage.make(options);
-}
-
-const COLLECTION_NAME = 'items';
-
-async function addToCollection(obj: any) {
-  const config = getConfig();
-  const name = COLLECTION_NAME;
-  const ref = await admin.firestore().collection(name).add(obj);
-  return {
-    id: ref.id,
-    collection: name,
-    doc: obj
-  };
-}
-
-
-export const newImage = functions.https.onRequest(async (request, response) => {
-  const result = await generateImage({})
-  console.log('fakeImage.make result', result);
-  response.send(result);
-});
-
-
-export const addDocument = functions.https.onRequest(async (req, res) => {
-  console.log('addDocument query', req.query);
-
-  if (! ('name' in req.query)) {
-    res.status(400).send({ error: "name not given" })
-    return null
-  }
-
-  if (! ('value' in req.query)) {
-    res.status(400).send({ error: "value not given" })
-    return null
-  }
-
-  const name = req.query.name;
-  const value = req.query.value;
-  const data = { name: value };
-
-  try {
-    const result = await addToCollection(data)
-    res.json(result);
-  }
-  catch (e) {
-    res.status(500).send(e)
-  }
-});
-
-
-export const addFake = functions.https.onRequest(async (req, res) => {
-  console.log('addFake query', req.query);
-  const name = req.query.name || 'text';
-  const value = faker.hacker.phrase();
-  const data = { name: value };
-
-  try {
-    const result = await addToCollection(data);
-    res.json(result);
-  }
-  catch (e) {
-    res.status(500).send(e)
-  }
-});
 
 function randomPrice() {
   // 5-25
   const base_price = (Math.floor(Math.random() * 5) + 1)*5;
   return base_price + 0.99
 }
+
+
+export const newImage = functions.https.onRequest(async (request, response) => {
+  const imageMaker = new ImageMaker();
+  const result = await imageMaker.make({});
+  console.log('fakeImage.make result', result);
+  response.send(result);
+});
+
+
 export const addFakePoster = functions.https.onRequest(async (req, res) => {
   console.log('addFakePoster query', req.query);
-  const imageOptions = {}
-  imageOptions['d'] = req.query['d'];
+
+  const color = req.query['color'] || 'black'
+
+  const imageOptions = {
+    d: req.query['d'],
+    color: color
+  }
+
   const noun = faker.company.bsNoun();
   const verb = faker.company.bsBuzz();
   const adjective = faker.company.bsAdjective();
-  const obj = {
-    message: `${verb} ${adjective} ${noun}`,
-    color: 'black',
-    topic: noun
-  };
+  // const obj = {
+  //   message: `${verb} ${adjective} ${noun}`,
+  //   color: color,
+  //   topic: noun
+  // };
+
+  const itemsColl = admin.firestore().collection('items');
+  const docRef = itemsColl.doc();
+
+  let imageResults: { localPath: string };
 
   try {
-    const imageResults = await generateImage(imageOptions);
-    console.log('fakeImage.make results:', imageResults);
+    const imageMaker = new ImageMaker();
+    imageResults = await imageMaker.make(imageOptions);
+    console.log('imageMaker.make results:', imageResults);
 
-    await Promise.all(imageResults.map(imageData => {
-      console.log('imageData:', imageData);
-      obj['path'] = imageData['path'];
-      obj['color'] = imageData['color'];
-      obj['price'] = randomPrice();
-      return addToCollection(obj);
-    }))
-    res.json(imageResults);
+    // Upload to Storage with the same name as the id of the doc to be created
+    const storagePath = `images/${docRef.id}.png`;
+    const uploadOptions = { destination: storagePath };
+    const bucket = admin.storage().bucket();
+    const files = await bucket.upload(imageResults.localPath, uploadOptions);
+
+    // Generate a public download URL
+    const downloadUrl = await files[0].getSignedUrl({
+      action: 'read',
+      expires: '2099-01-01'
+    });
+
+    // Add the doc data
+    const docData = {
+      topic: noun,
+      message: `${verb} ${adjective} ${noun}`,
+      price: randomPrice(),
+      color: color,
+      storagePath: storagePath,
+      url: downloadUrl[0]
+    }
+    await docRef.set(docData);
+
+    res.json(docData);
   }
-  catch (e) {
-    res.status(500).send(e)
+  catch (err) {
+    console.error(err)
+    res.status(500).send(err)
+  }
+  finally {
+    console.log(`Deleting ${imageResults.localPath}`)
+    fs.unlinkSync(imageResults.localPath)
   }
 });
